161ef319ec24fbd15924cd7a7cc92a8b11fd4bbc
transparenceco <bruno@transparence.co>
2021-12-28 21:48:12 -0500

    Created version a8 - separate pages

---
+++ a8/barba.js
@@ -0,0 +1,21 @@
+const animationEnter = (container) => {
+    return gsap.from(container, {autoAlpha:0, duration:.5, clearProps:'all', ease:'out'});
+}
+
+const animationLeave = (container) => {
+    return gsap.to(container, { autoAlpha:0, duration:.5, clearProps:'all', ease:'none'});
+}
+
+barba.init ({
+    transitions: [
+        {   
+            once({next}){
+                animationEnter(next.container);
+            },
+            leave: ({current}) => animationLeave(current.container),
+            enter({next}){
+                animationEnter(next.container);
+            }
+        }
+    ]
+})
